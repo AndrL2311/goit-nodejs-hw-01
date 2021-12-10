4a4c39c02fcb2064241b471ca30fa283e9b70306
Andrii Lypovetskyi <li_andr@ukr.net>
2021-12-10 20:57:36 +0200

    Добавил listContacts и getContactById

--- contacts.js
+++ contacts.js
@@ -1,16 +1,41 @@
-// const fs = require('fs').promises;
+const fs = require("fs").promises;
 const path = require("path");
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
-console.log(contactsPath);
-// // TODO: задокументировать каждую функцию
-// function listContacts() {
-//   // ...твой код
-// }
+// console.log(contactsPath);
 
-// function getContactById(contactId) {
-//   // ...твой код
-// }
+/*
+1. Получить все контакты. --> listContacts
+2. Получить один контакт по id. --> getContactById
+3. Удалить контакт по id. --> removeContact
+4. Добавить контакт в список. --> addContact
+5. Обновить контакт по id. --> changeContact
+*/
+
+const listContacts = async () => {
+  //   console.log(contactsPath);
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const getContactById = async (id) => {
+  try {
+    const contacts = await listContacts();
+    // console.log(id);
+    const contact = contacts.find((item) => item.id === String(id));
+    if (!contact) {
+      return null;
+    }
+    return contact;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 // function removeContact(contactId) {
 //   // ...твой код
@@ -19,3 +44,10 @@ console.log(contactsPath);
 // function addContact(name, email, phone) {
 //     // ...твой код
 // }
+
+module.exports = {
+  listContacts,
+  getContactById,
+  // removeContact,
+  // addContact,
+};
--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Welcome the HEll!!!");
+const argv = require("yargs").argv;
+const contactOperations = require("./contacts");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await contactOperations.listContacts();
+      console.log(contacts);
+      break;
+
+    case "get":
+      const contact = await contactOperations.getContactById(id);
+      console.log(contact);
+      break;
+
+    // case "add":
+    //   // ... name email phone
+    //   break;
+
+    // case "remove":
+    //   // ... id
+    //   break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
