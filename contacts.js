b02943c99a9cedbd62f2bdac3987088d9e6a8e74
Andrii Lypovetskyi <li_andr@ukr.net>
2021-12-11 12:48:11 +0200

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -9,8 +9,9 @@ const contactsPath = path.join(__dirname, "db", "contacts.json");
 1. Получить все контакты. --> listContacts
 2. Получить один контакт по id. --> getContactById
 3. Добавить контакт в список. --> addContact
-4. Удалить контакт по id. --> removeContact
-5. Обновить контакт по id. --> changeContact
+4. Обновить контакт по id. --> changeContact
+5. Удалить контакт по id. --> removeContact
+
 */
 
 const listContacts = async () => {
@@ -42,7 +43,7 @@ const updateContacts = async (contacts) => {
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 };
 
-const addContact = async ({name, email, phone}) => {
+const addContact = async ({ name, email, phone }) => {
   try {
     const newContact = { id: v4(), name, email, phone };
     const contacts = await listContacts();
@@ -54,7 +55,7 @@ const addContact = async ({name, email, phone}) => {
   }
 };
 
-const updateById = async ({id, name, email, phone}) => {
+const updateById = async ({ id, name, email, phone }) => {
   try {
     const contacts = await listContacts();
 
@@ -76,7 +77,7 @@ const updateById = async ({id, name, email, phone}) => {
   }
 };
 
-const removeContact = async ({id}) => {
+const removeContact = async ({ id }) => {
   try {
     const contacts = await listContacts();
     const idx = contacts.findIndex((item) => item.id === String(id));
